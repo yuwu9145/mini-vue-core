468a629c9ff2774e4795886809119f113117d3ff
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2022-09-22 21:53:07 +1000

    Add scheduler test case

--- packages/reactivity/reactive.spec.js
+++ packages/reactivity/reactive.spec.js
@@ -1,7 +1,7 @@
 import { reactive, effect } from './reactive.js'
 
 
-test('basic reactive', () => {
+test('STEP ONE: basic reactive', () => {
   // initial obj
   const obj = { 
     foo: 1,
@@ -20,4 +20,32 @@ test('basic reactive', () => {
   expect(sum).toBe(4)
   proxiedObj.foo = 3
   expect(sum).toBe(5)
-});
+})
+
+// Simplied version based on vue core source code 
+// https://github.com/vuejs/core/blob/8772a01a9280b1591e781e20741d32e2f9a836c8/packages/reactivity/__tests__/effect.spec.ts
+test('STEP TWO: scheduler', () => {
+  let dummy
+  let run
+  const scheduler = jest.fn(() => {
+    run = runner
+  })
+  const obj = reactive({ foo: 1 })
+  const runner = effect(
+    () => {
+      dummy = obj.foo
+    },
+    { scheduler }
+  )
+  expect(scheduler).not.toHaveBeenCalled()
+  expect(dummy).toBe(1)
+  // should be called on first trigger
+  obj.foo++
+  expect(scheduler).toHaveBeenCalledTimes(1)
+  // should not run yet
+  expect(dummy).toBe(1)
+  // manually run
+  run()
+  // should have run
+  expect(dummy).toBe(2)
+})
