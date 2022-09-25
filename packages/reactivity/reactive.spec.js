58728fa042832463de35a4833a70712063e52f62
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2022-09-26 08:18:56 +1000

    Add ref test caseus

--- packages/reactivity/reactive.spec.js
+++ packages/reactivity/reactive.spec.js
@@ -116,3 +116,84 @@ xit('should trigger effect', () => {
   value.foo = 1
   expect(dummy).toBe(1)
 })
+
+// real test case from vue core source code 
+describe('STEP SIX: reactivity/ref', () => {
+  it('should hold a value', () => {
+    const a = ref(1)
+    expect(a.value).toBe(1)
+    a.value = 2
+    expect(a.value).toBe(2)
+  })
+
+  it('should be reactive', () => {
+    const a = ref(1)
+    let dummy
+    let calls = 0
+    effect(() => {
+      calls++
+      dummy = a.value
+    })
+    expect(calls).toBe(1)
+    expect(dummy).toBe(1)
+    a.value = 2
+    expect(calls).toBe(2)
+    expect(dummy).toBe(2)
+    // same value should not trigger
+    a.value = 2
+    expect(calls).toBe(2)
+  })
+
+  it('should make nested properties reactive', () => {
+    const a = ref({
+      count: 1
+    })
+    let dummy
+    effect(() => {
+      dummy = a.value.count
+    })
+    expect(dummy).toBe(1)
+    a.value.count = 2
+    expect(dummy).toBe(2)
+  })
+
+  it('should work without initial value', () => {
+    const a = ref()
+    let dummy
+    effect(() => {
+      dummy = a.value
+    })
+    expect(dummy).toBe(undefined)
+    a.value = 2
+    expect(dummy).toBe(2)
+  })
+
+  it('should work like a normal property when nested in a reactive object', () => {
+    const a = ref(1)
+    const obj = reactive({
+      a,
+      b: {
+        c: a
+      }
+    })
+
+    let dummy1
+    let dummy2
+
+    effect(() => {
+      dummy1 = obj.a
+      dummy2 = obj.b.c
+    })
+
+    const assertDummiesEqualTo = (val) =>
+      [dummy1, dummy2].forEach(dummy => expect(dummy).toBe(val))
+
+    assertDummiesEqualTo(1)
+    a.value++
+    assertDummiesEqualTo(2)
+    obj.a++
+    assertDummiesEqualTo(3)
+    obj.b.c++
+    assertDummiesEqualTo(4)
+  })
+})
