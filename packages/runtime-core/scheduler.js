37003ebf93c13fd8a83e97b51c9b5e44ecc32f0c
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2022-11-20 15:18:21 +1100

    WIP of scheduler

--- packages/runtime-core/scheduler.js
+++ packages/runtime-core/scheduler.js
@@ -1,1 +1,18 @@
 const jobQueue = new Set()
+
+const p = Promise.resolve()
+
+let isFlushing = false
+
+function flushJob() {
+
+  if (isFlushing) return
+
+  isFlushing = true
+
+  p.then(() => {
+    jobQueue.forEach(job => job())
+  }).finally(() => {
+    isFlushing = false
+  })
+}
